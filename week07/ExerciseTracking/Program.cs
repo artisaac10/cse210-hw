0092173996404a97dce10afb08fa7907c7834e93
Your Name <your_email@example.com>
2026-08-10 18:27:07 -0500

    finished code, submitting final project

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+
+        Running running = new Running(
+            new DateTime(2022, 11, 3),
+            30,
+            3.0
+        );
+
+        Cycling cycling = new Cycling(
+            new DateTime(2022, 11, 4),
+            45,
+            15.0
+        );
+
+        Swimming swimming = new Swimming(
+            new DateTime(2022, 11, 5),
+            30,
+            30
+        );
+
+        activities.Add(running);
+        activities.Add(cycling);
+        activities.Add(swimming);
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
